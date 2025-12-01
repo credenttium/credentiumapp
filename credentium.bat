7aa23cdb2a5d55bbeb805eaa7b02fff052b0d69b
José Quintinno <credentium@outlook.com.br>
2025-12-01 13:59:39 -0300

    build: Gerar Aplicativo Android

---
+++ credentium.bat
@@ -0,0 +1,1 @@
+npx cap run android --target=Medium_Phone_API_36.1  --no-sync
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: "http://192.168.15.14:8080",
+  api_url: "http://10.0.2.2:8080",
 };
